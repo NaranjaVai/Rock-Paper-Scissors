00a0f20e6f04aea22c039ef40d2f78c9afcfbc9e
Angel Mendez <oangel0590@gmail.com>
2022-08-13 12:45:26 -0300

    Agregando local y DOM

--- main.js
+++ main.js
@@ -1,37 +1,15 @@
 // Simulador de rock, paper, scissors
-/* 
-class Jugador {
-    constructor(nombre,historialJugadas){
-        this.nombre = nombre;
-        this.historialJugadas = historialJugadas;
-    }
 
-}    */
-/* 
-function verificar(){
-    let choice
-   do{
-     choice = prompt('Juego de piedra, papel o tijera' + '\n Elija la opcion que va a usar').toLowerCase();
-    } while((choice != 'piedra') && (choice != 'papel') && (choice != 'tijera'));
-    return choice;
-}
- */
-/* 
-function verificarSiNo(text){
-    let a
-    do{
-        a = prompt(text).toLowerCase();
-       } while((a != 'si') && (a != 'no'));
-       return a;
-}
- */
-//const texto = ['Quiere volver a jugar? (Si o No)', 'Desea ver el Historial de partidas? (Si o No)']
 
 const opciones = document.querySelectorAll('button')
 const opcionUser = document.getElementById('valueUser')
 const opcionCPU = document.getElementById('valueCPU')
 const result = document.getElementById('result')
-let valorUser
+const botonHistorial = document.getElementById('historial')
+const reset = document.getElementById('reset')
+let valorUser,valorHistorial,botonh
+let historial =[];
+
 
 function resultado(decision) {
     let valor, result
@@ -48,76 +26,75 @@ function resultado(decision) {
             valor = 'tijera';
             break;
     }
-    opcionCPU.innerHTML = valor
-    if (decision == valor){
-        return result = 'Empate'
-    }
-
-    //opcion piedra
-    if((decision == 'piedra') && (valor == 'tijera')){
-        return result = 'Victoria'
-    } else if ((decision == 'piedra') && (valor == 'papel')){
-        return result = 'Derrota'
-    }
-
-    //opcion papel
-    if((decision == 'papel') && (valor == 'piedra')){
-        return result = 'Victoria'
-    } else if ((decision == 'papel') && (valor == 'tijera')){
-        return result = 'Derrota'
-    }
-
-    //opcion tijera
-    if((decision == 'tijera') && (valor == 'papel')){
-        return result = 'Victoria'
-    } else if ((decision == 'tijera') && (valor == 'piedra')){
-        return result = 'Derrota'
-    }
+    opcionCPU.innerHTML = valor;
+    (decision == valor) ? result = 'Empate' :
+    ((decision == 'piedra') && (valor == 'tijera')) ? result = 'Victoria' :  ((decision == 'piedra') && (valor == 'papel')) ? result = 'Derrota' :
+    ((decision == 'papel') && (valor == 'piedra')) ?  result = 'Victoria' :  ((decision == 'papel') && (valor == 'tijera')) ? result = 'Derrota' :
+    ((decision == 'tijera') && (valor == 'papel')) ?  result = 'Victoria' :  ((decision == 'tijera') && (valor == 'piedra')) ? result = 'Derrota' : null
+    localStorage.setItem('ValorCPU', valor);
+    localStorage.setItem('ValorUser', decision);
+    return result;
 }
 
+opcionCPU.innerHTML = localStorage.getItem('ValorCPU');
+opcionUser.innerHTML = localStorage.getItem('ValorUser')
+result.innerHTML = localStorage.getItem('Resultado')
+botonh = document.createElement('h2');
 
-
-let valorHistorial
-const historial =[];
-const botonHistorial = document.getElementById('historial')
-
-
-
+if ((localStorage.getItem('Victorias') != null) && (localStorage.getItem('Derrotas') != null) && ((localStorage.getItem('Empates') != null))){
+        botonh.innerHTML = `    <h2> Usted ha ganado:   ${(localStorage.getItem('Victorias'))} </h2>
+                                <h2> Usted ha perdido:  ${(localStorage.getItem('Derrotas'))} </h2>
+                                <h2> Usted ha empatado: ${(localStorage.getItem('Empates'))} </h2>`
+                                for(i=0; i< (localStorage.getItem('Victorias')); i++){ historial.push('Victoria')}
+                                for(i=0; i< (localStorage.getItem('Derrotas')); i++){ historial.push('Derrota')}
+                                for(i=0; i< (localStorage.getItem('Empates')); i++){ historial.push('Empate')}
+                                console.log(historial)
+                    }       
 
 opciones.forEach(opcion => opcion.addEventListener('click', (e) => {
     valorUser = e.target.id
-    console.log(valorUser)
     valorHistorial = resultado(valorUser);
+    
     switch (valorHistorial){
         case 'Victoria':
             historial.push ('Victoria');
             result.innerHTML = 'Ganaste!'
+            localStorage.setItem('Resultado', result.innerHTML);
             break;
         case 'Derrota':
             historial.push ('Derrota');
             result.innerHTML = 'Perdiste!'
+            localStorage.setItem('Resultado', result.innerHTML);
             break;
         case 'Empate':
             historial.push ('Empate');
             result.innerHTML = 'Es un empate!'
+            localStorage.setItem('Resultado', result.innerHTML);
             break;
     }
 
-    console.log(historial)   
+    botonh.innerHTML = `  <h2> Usted ha ganado :  ${historial.filter(valor => valor == 'Victoria').length} </h2>
+                          <h2> Usted ha perdido:  ${historial.filter(valor => valor == 'Derrota').length} </h2>
+                          <h2> Usted ha empatado: ${historial.filter(valor => valor == 'Empate').length} </h2>`
+
+    localStorage.setItem('Victorias', historial.filter(valor => valor == 'Victoria').length);
+    localStorage.setItem('Derrotas', historial.filter(valor => valor == 'Derrota').length);
+    localStorage.setItem('Empates', historial.filter(valor => valor == 'Empate').length);
 }))
 
-
-    
 botonHistorial.addEventListener('click', (e) => {
     e.preventDefault();
-    let informaV = historial.filter(valor => valor == 'Victoria')
-    let informaD = historial.filter(valor => valor == 'Derrota')
-    let informaE = historial.filter(valor => valor == 'Empate')
+    if (botonHistorial.querySelectorAll('h2').length === 0){
+    botonh.classList.add('historial');
+    botonHistorial.appendChild(botonh);
+}
+})
 
-    botonHistorial.innerHTML = `<h2> Usted ha ganado :  ${informaV.length} </h2>
-                                <h2> Usted ha perdido:  ${informaD.length} </h2>
-                                <h2> Usted ha empatado: ${informaE.length} </h2>`
 
+reset.addEventListener('click', (e) => {
+    e.preventDefault();
+    historial = [];
+    botonh.innerHTML;
 })
 
 
