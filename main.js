825aaf3206898a10ccd49196bf57516fa108a81c
Angel Mendez <oangel0590@gmail.com>
2022-09-01 18:26:36 -0300

    agregando fetch y acomodando para el tp final

--- main.js
+++ main.js
@@ -9,37 +9,95 @@ const botonHistorial = document.getElementById('historial')
 const reset = document.getElementById('reset')
 let valorUser,valorHistorial,botonh
 let historial =[];
+let picU = document.getElementById('PicUser');
+let picC = document.getElementById('PicCPU');
+let piedra, papel, tijera
+let tijeraNames = ['scyther','binacle','scizor','barbaracle']
 
+
+    fetch('https://pokeapi.co/api/v2/pokemon/kartana') //Como el pokemon papel en este caso
+    .then(response => response.json())
+    .then(data => {
+        papel = data.sprites.front_default;
+    })
+    
+    
+    let randomizerPiedra = () => {
+    fetch('https://pokeapi.co/api/v2/type/rock')  // interpreta a la roca, aunque algunas veces salen otros
+    .then(response => response.json())
+    .then(out => {
+        let ub = out.pokemon[Math.floor(Math.random() * 93)];
+        fetch(`${ub.pokemon.url}`)
+            .then(response => response.json())
+            .then(data =>{
+                console.log(data)
+                piedra = data.sprites.front_default; 
+            })
+    })}
+    
+    let randomizerTijera = ()=> {             
+    fetch(`https://pokeapi.co/api/v2/pokemon/${tijeraNames[Math.floor(Math.random() * 4)]}`)
+    .then(response => response.json())
+    .then(data => {
+        tijera = data.sprites.front_default;
+    })}
+
+    // https://pokeapi.co/api/v2/pokemon/binacle
+    // https://pokeapi.co/api/v2/pokemon/scizor
+    // https://pokeapi.co/api/v2/pokemon/barbaracle
+    
+    
 
 function resultado(decision) {
     let valor, result
     let numero = Math.floor(Math.random() * 3);
-    opcionUser.innerHTML = decision
+    //opcionUser.innerHTML = decision
     switch (numero){
         case 0:
             valor = 'piedra';
+            picC.src = piedra;
             break;
         case 1:
             valor = 'papel';
+            picC.src = papel;
             break;
         case 2:
             valor = 'tijera';
+            picC.src = tijera;
             break;
     }
-    opcionCPU.innerHTML = valor;
+
+    //opcionCPU.innerHTML = valor;
     (decision == valor) ? result = 'Empate' :
     ((decision == 'piedra') && (valor == 'tijera')) ? result = 'Victoria' :  ((decision == 'piedra') && (valor == 'papel')) ? result = 'Derrota' :
     ((decision == 'papel') && (valor == 'piedra')) ?  result = 'Victoria' :  ((decision == 'papel') && (valor == 'tijera')) ? result = 'Derrota' :
     ((decision == 'tijera') && (valor == 'papel')) ?  result = 'Victoria' :  ((decision == 'tijera') && (valor == 'piedra')) ? result = 'Derrota' : null
     localStorage.setItem('ValorCPU', valor);
     localStorage.setItem('ValorUser', decision);
+
+    switch (decision){
+        case 'piedra':
+            picU.src = piedra;
+            break;
+        case 'papel':
+            picU.src = papel;
+            break;
+        case 'tijera':
+            picU.src = tijera;
+            break;
+    }
+
     return result;
 }
 
-opcionCPU.innerHTML = localStorage.getItem('ValorCPU');
-opcionUser.innerHTML = localStorage.getItem('ValorUser')
+//opcionCPU.innerHTML = localStorage.getItem('ValorCPU');
+//opcionUser.innerHTML = localStorage.getItem('ValorUser')
 result.innerHTML = localStorage.getItem('Resultado')
 botonh = document.createElement('h2');
+
+
+
+
 
 if ((localStorage.getItem('Victorias') != null) && (localStorage.getItem('Derrotas') != null) && ((localStorage.getItem('Empates') != null))){
         botonh.innerHTML = `    <h2> Usted ha ganado:   ${(localStorage.getItem('Victorias'))} </h2>
@@ -54,6 +112,8 @@ if ((localStorage.getItem('Victorias') != null) && (localStorage.getItem('Derrot
 opciones.forEach(opcion => opcion.addEventListener('click', (e) => {
     valorUser = e.target.id
     valorHistorial = resultado(valorUser);
+    randomizerTijera();
+    randomizerPiedra();
     
     switch (valorHistorial){
         case 'Victoria':
